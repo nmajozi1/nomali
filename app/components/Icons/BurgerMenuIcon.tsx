bd29dca68c094a135f338321c29f309cff7f61d3
Ntokozo Majozi <ntokozomajozi@Ntokozos-MacBook-Air.local>
2025-03-24 06:08:03 +0200

    try different size

--- app/components/Icons/BurgerMenuIcon.tsx
+++ app/components/Icons/BurgerMenuIcon.tsx
@@ -6,6 +6,7 @@ const BurgerMenuIcon = () => {
       viewBox="0 0 24 24" 
       fill="none" 
       xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
     >
       <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
